7f3d17c2a245c1dbdb4cc9136869c9e2f5ffdd21
trif-hub <gncrvpa@gmail.com>
2025-04-11 06:42:44 +0300

    Add files via upload
    решения модуля 3

---
+++ 2-3.py
@@ -0,0 +1,7 @@
+n = int(input('какое число? '))
+n = str(n)
+ttl = 0
+for i in n:
+   ttl += int(i)
+print(ttl, 'сумма цифр числа')
+   
---
+++ 2-6.py
@@ -0,0 +1,5 @@
+d = {'name1':'id1', 'name2':'id2', 'name3':'id3'}
+print (d)
+d.clear()
+d.update({'id1':'name1', 'id2':'name2', 'id3':'name3'})
+print (d)
